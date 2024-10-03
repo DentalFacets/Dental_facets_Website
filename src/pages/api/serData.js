ca52a1c2d880fc38f5c02d85511c9d21100ca28d
ZOD-IAC <harshitvarun89@gmail.com>
2024-10-03 17:41:51 +0530

    cors fix

--- src/pages/api/serData.js
+++ src/pages/api/serData.js
@@ -1,8 +1,20 @@
 import servicesData from "@/Data/services.json";
+import Cors from "cors";
+import initMiddleware from "@/lib/init-middleware";
 
-export default function handler(req, res) {
+// Initialize the cors middleware to allow requests from your custom domain
+const cors = initMiddleware(
+  Cors({
+    origin: "https://www.dentalfacets.in", // Allow only this origin
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export default async function handler(req, res) {
   const { head } = req.query;
+  // Run CORS middleware before your actual API logic
 
+  await cors(req, res);
   // If a key is provided, filter the data by key
   if (head) {
     const filteredService = servicesData.find(
